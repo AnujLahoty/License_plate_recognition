961f4a4384e1df643a71dd8450ae7eae8fd54c6f
Anuj M Lahoty <48029649+AnujLahoty@users.noreply.github.com>
2020-06-18 05:57:05 +0530

    Update numplate_recognition_detection_using_images.py

--- numplate_recognition_detection_using_images.py
+++ numplate_recognition_detection_using_images.py
@@ -1,28 +1,136 @@
-'''
-REFERENCE : DHARUN'S REPO
-IMPORTANT OBSERVATIONS
-Below script works fine in cropping out the license plate when the resolution of the images are quiet high.
-Although it is not able to extract the characters at all using the pytesseract approach.
-Some alternate approach should be employed.Try to implement the AI based license plate detector's project code's 
-   aproach in oredr to get the characters from the number plates.
-
-'''
-
 import numpy as np
 import os
 import sys
 import tensorflow as tf
 from PIL import Image
 import cv2
-import pytesseract
-
-from custom_plate import allow_needed_values as anv 
-from custom_plate import do_image_conversion as dic
+from tensorflow.python.keras.models import load_model
+import label_map_util
+import visualization_utils as vis_util
 
 sys.path.append("..")
 
-import label_map_util
-import visualization_utils as vis_util
+def fix_dimension(img): 
+  new_img = np.zeros((28,28,3))
+  for i in range(3):
+    new_img[:,:,i] = img
+  return new_img
+  
+def show_results():
+    dic = {}
+    characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for i,c in enumerate(characters):
+        dic[i] = c
+
+    output = []
+    for i,ch in enumerate(char): #iterating over the characters
+        img_ = cv2.resize(ch, (28,28))
+        img = fix_dimension(img_)
+        img = img.reshape(1,28,28,3) #preparing image for the model
+        y_ = model.predict_classes(img)[0] #predicting the class
+        character = dic[y_] #
+        output.append(character) #storing the result in a list
+        
+    plate_number = ''.join(output)
+    
+    return plate_number
+
+
+# Match contours to license plate or character template
+
+def find_contours(dimensions, img) :
+
+    # Find all contours in the image
+    cntrs, _ = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Retrieve potential dimensions
+    lower_width = dimensions[0]
+    upper_width = dimensions[1]
+    lower_height = dimensions[2]
+    upper_height = dimensions[3]
+    
+    # Check largest 5 or  15 contours for license plate or character respectively
+    cntrs = sorted(cntrs, key=cv2.contourArea, reverse=True)[:15]
+
+    x_cntr_list = []
+    img_res = []
+    for cntr in cntrs :
+        #detects contour in binary image and returns the coordinates of rectangle enclosing it
+        intX, intY, intWidth, intHeight = cv2.boundingRect(cntr)
+        
+        #checking the dimensions of the contour to filter out the characters by contour's size
+        if intWidth > lower_width and intWidth < upper_width and intHeight > lower_height and intHeight < upper_height :
+            x_cntr_list.append(intX) #stores the x coordinate of the character's contour, to used later for indexing the contours
+
+            char_copy = np.zeros((44,24))
+            #extracting each character using the enclosing rectangle's coordinates.
+            char = img[intY:intY+intHeight, intX:intX+intWidth]
+            char = cv2.resize(char, (20, 40))
+
+            # Make result formatted for classification: invert colors
+            char = cv2.subtract(255, char)
+            
+            cv2.imshow('individual_character_image',char)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            # Resize the image to 24x44 with black border
+            char_copy[2:42, 2:22] = char
+            char_copy[0:2, :] = 0
+            char_copy[:, 0:2] = 0
+            char_copy[42:44, :] = 0
+            char_copy[:, 22:24] = 0
+
+            img_res.append(char_copy) #List that stores the character's binary image (unsorted)
+
+    #Return characters on ascending order with respect to the x-coordinate (most-left character first)
+    
+    #arbitrary function that stores sorted list of character indeces
+    indices = sorted(range(len(x_cntr_list)), key=lambda k: x_cntr_list[k])
+    img_res_copy = []
+    for idx in indices:
+        img_res_copy.append(img_res[idx])# stores character images according to their index
+    img_res = np.array(img_res_copy)
+
+    return img_res
+
+# Find characters in the resulting images
+def segment_characters(image) :
+
+    # Preprocess cropped license plate image
+    img = cv2.resize(image, (333, 75))
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img_binary = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    img_erode = cv2.erode(img_binary, (3,3))
+    img_dilate = cv2.dilate(img_erode, (3,3))
+    
+    cv2.imshow('binary_image_of_numplate',img_dilate)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    
+    LP_WIDTH = img_dilate.shape[0]
+    LP_HEIGHT = img_dilate.shape[1]
+    
+    # Make borders white
+    img_dilate[0:3,:] = 255
+    img_dilate[:,0:3] = 255
+    img_dilate[72:75,:] = 255
+    img_dilate[:,330:333] = 255
+    
+    # Estimations of character contours sizes of cropped license plates
+    dimensions = [LP_WIDTH/6, LP_WIDTH/2, LP_HEIGHT/10, 2*LP_HEIGHT/3]
+
+    # Get contours within cropped license plate
+    char_list = find_contours(dimensions, img_dilate)
+
+    return char_list
+
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
 
 MODEL_NAME = 'numplate'
 PATH_TO_CKPT = MODEL_NAME + '/graph-200000/frozen_inference_graph.pb'
@@ -43,39 +151,40 @@ label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
-
 PATH_TO_TEST_IMAGES_DIR = 'png_tesseract/test_ram'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'i_{}.jpg'.format(i)) for i in range(1, 3) ]
+TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, filename) for filename in os.listdir(PATH_TO_TEST_IMAGES_DIR) ]
 TEST_ANUJ=os.path.join('numplate')
 count = 0
 
 '''
-Below code from 57-111 is responsible for the object detection part.  
+Here we are dealing with the 2 different models.
+
+MODEL - 1 : For detecting ROI (License plate region) through object detection API by custom object detection approach (Dharun's approach).
+MODEL - 2 : For detecting the characters of the licensse plate.
+ 
 '''
 
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    
     # Each box represents a part of the image where a particular object was detected.
     detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    
     # Each score represent how level of confidence for each of the objects.
     # Score is shown on the result image, together with the class label.
+    
     detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
     for image_path in TEST_IMAGE_PATHS:
       image = Image.open(image_path) 
       image_np = load_image_into_numpy_array(image)
+     
       # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
       image_np_expanded = np.expand_dims(image_np, axis=0)
       
-      # Actual detection.
+      # Actual detection process.
       (boxes, scores, classes, num) = sess.run(
           [detection_boxes, detection_scores, detection_classes, num_detections],
           feed_dict={image_tensor: image_np_expanded})
@@ -88,22 +197,6 @@ with detection_graph.as_default():
       cropped_image = tf.image.crop_to_bounding_box(image_np, int(yminn), int(xminn),int(ymaxx - yminn), int(xmaxx - xminn))
       img_data = sess.run(cropped_image)
       
-      '''
-      below lines 83-88 are trying to extract the characters from the cropped number plate
-      i.e the number plate which only has image of the characters.It won't have any car's 
-      objects in it.
-      '''
-      count = 0
-      filename = dic.yo_make_the_conversion(img_data, count)
-      pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-      text = pytesseract.image_to_string(Image.open(filename),lang="eng") 
-      print("Text", text)
-      print('CHARACTER RECOGNITION : ',anv.catch_rectify_plate_characters(text))
-      
-      '''
-      below lines 90-97 would put bounding box around the images and label(numplate) the detected number plate
-      along with the score.
-      '''
       # Visualization of the results of a detection.
       vis_util.visualize_boxes_and_labels_on_image_array(
           image_np,
@@ -113,9 +206,86 @@ with detection_graph.as_default():
           category_index,
           use_normalized_coordinates=True,
           line_thickness=5)
+     
+      cv2.imshow('image_np',image_np)     
+      cv2.imshow('img_data',img_data)
+      cv2.waitKey(0)
+      cv2.destroyAllWindows()
       
-cv2.imshow('image_np',image_np)     
-cv2.imshow('img_data',img_data)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+      model = load_model('my_model.h5') # This would load the entire model for the detection of the LP characters.
+      
+      char = segment_characters(img_data)
+     
 
+      print(show_results())
+
+###############################################################################################################################################
+      
+'''
+MISCELLANEOUS SECTION
+'''
+      
+      
+
+'''
+
+Below is the given skeleton of our model which I have used for training and saved all of its
+parameters to the my_model.h5 file.
+
+Training the model
+
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255, width_shift_range=0.05, height_shift_range=0.05)
+
+train_generator = train_datagen.flow_from_directory(
+        'data/train',  # this is the target directory
+        target_size=(28,28),  # all images will be resized to 28x28
+        batch_size=1,
+        class_mode='categorical')
+
+validation_generator = train_datagen.flow_from_directory(
+        'data/val',  # this is the target directory
+        target_size=(28,28),  # all images will be resized to 28x28
+        batch_size=1,
+        class_mode='categorical')
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Flatten, MaxPooling2D, Dropout, Conv2D
+from tensorflow.python.keras import optimizers
+
+model = Sequential()
+model.add(Conv2D(32, (24,24), input_shape=(28, 28, 3), activation='relu', padding='same'))
+# model.add(Conv2D(32, (20,20), input_shape=(28, 28, 3), activation='relu', padding='same'))
+# model.add(Conv2D(32, (20,20), input_shape=(28, 28, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.4))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(36, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(lr=0.00001), metrics=['accuracy'])
+
+batch_size = 1
+model.fit_generator(
+      train_generator,
+      steps_per_epoch = train_generator.samples // batch_size,
+      validation_data = validation_generator, 
+      validation_steps = validation_generator.samples // batch_size,
+      epochs = 20)
+
+model.save('my_model.h5') #This would also save all the variables and also the state of the optimizers.
+'''
+
+
+'''
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,6))
+for i,ch in enumerate(char):
+    img = cv2.resize(ch, (28,28))
+    plt.subplot(3,4,i+1)
+    plt.imshow(img,cmap='gray')
+    plt.title('predicted: {}'.format(show_results()[i]))
+    plt.axis('off')
+    plt.show()
+'''
